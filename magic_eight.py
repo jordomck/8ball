6a9c1ec5b2b0f38a849217e4076dcb8482e04a45
jordomck <jordomck@umich.edu>
2019-09-17 16:39:03 -0400

    added game structure and question checking

--- magic_eight.py
+++ magic_eight.py
@@ -1,4 +1,21 @@
 
 def getQuestion():
     question = input("What is your question? ")
+    if(question == "quit"):
+        exit()
+    
     return question
+
+def checkQuestion(question):
+    isQuestion = question[-1] == '?'
+    if not isQuestion:
+        print("I'm sorry, I can only answer questions.")
+    return isQuestion
+
+
+print("Welcome to the magic 8 program!")
+while(True):
+    question = getQuestion()
+    if(checkQuestion(question)):
+        print("here's an answer: ")
+        #print answer 
